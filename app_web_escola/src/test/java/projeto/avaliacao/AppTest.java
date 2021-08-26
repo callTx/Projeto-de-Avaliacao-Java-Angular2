8946ee4aff31d1084d90c088c18f08ce87bd34f4
callTx <danielfdeus@gmail.com>
2021-08-26 10:08:12 -0300

    Adiciona pre projeto Java

---
+++ app_web_escola/src/test/java/projeto/avaliacao/AppTest.java
@@ -0,0 +1,20 @@
+package projeto.avaliacao;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
